2cdc8d9d2ecb337d9199879805f2035051fdac3e
Vexurt <israelsenen_@hotmail.com>
2024-12-15 02:52:56 -0600

    listones

--- app/views.py
+++ app/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
 
 from rest_framework import viewsets
 from .serializer import UrlSerializer, UrlSerializer2
-from .models import Url, BlackList
+from .models import Url, BlackList,WhiteList, Graylist
+import requests 
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -21,14 +21,65 @@ class UrlViewSet(viewsets.ModelViewSet):
 @api_view(['GET', 'POST'])
 def check_list(request):
     is_white_list = "aceptado"
-    result = checkBlackList(request.data.url)
+    url = "https://www.facebuook.com"
+    result = checkList(url)
+    print(result)
+
 
     return Response({
-        'status_scan': is_white_list, 
+        'status_scan': result, 
         'url': "www.leo.com",
     })
 
 
-def checkBlackList(url):
+def checkList(url):
 
-    return True
+    mydata = BlackList.objects.filter(url__url=url).first()
+    if not mydata:         
+         mydata = WhiteList.objects.filter(url__url=url).first()
+         if not mydata:
+             mydata =Graylist.objects.filter(url__url=url).first()
+             if not mydata:
+                 result= apiTerceros(url)
+                 print (result)
+                 return result
+             else:
+                 return "Gray List"
+         else:
+          return "White List"
+    else:
+        return "Black List"
+    
+    return ""
+
+def apiTerceros(url):
+    Key = "6c17899cc957d571c00ab9b01e4179fde72c9bf5980b94d26cc158c1f8be1a73"
+    payload = { "url":url }
+
+    headers = {
+        "accept": "application/json",
+        "x-apikey": Key,
+        "content-type": "application/x-www-form-urlencoded"
+
+     }
+    report_response = requests.post(url, data=payload, headers=headers)
+    report_id = report_response.json()["data"]["id"]
+    api_url = f"https://www.virustotal.com/api/v3/analyses/{report_id}"
+    headers = {
+        "accept": "application/json",
+        "x-apikey": "6c17899cc957d571c00ab9b01e4179fde72c9bf5980b94d26cc158c1f8be1a73"
+     }
+    response_consult = requests.get(api_url, headers=headers)
+    stats = response_consult.json()["data"]["attributes"]["stats"]["malicius"]
+    if stats > 0:
+        return "Black List"
+    else:
+        return "Indeterminado Virus total"
+    return ""
+
+
+
+
+
+
+        
